3360a426b5682d5cfe3973ea0e1915f23b7e266b
MauroGutMB <mgmbarros2006@gmail.com>
2025-10-08 17:23:39 -0300

    ce

--- C_dir/estrDados/10_25/07_10_25/atvd1.c
+++ C_dir/estrDados/10_25/07_10_25/atvd1.c
@@ -6,3 +6,41 @@
  * Esta função deve ter como protótipo:
  *    int dentroRet(Ponto *v1, Ponto *v2, Ponto *p);
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+  int v1;
+  int v2;
+} Ponto;
+
+void iniciarPonto(Ponto *ponto, int v1, int v2){
+  ponto->v1 = v1;
+  ponto->v2 = v2;
+}
+
+void liberarPonto(Ponto *ponto){
+  if(ponto == NULL){
+    return;
+  }
+  free(ponto);
+}
+
+int dentroRet(Ponto *v1, Ponto *v2, Ponto *p);
+
+int main(){
+
+  Ponto *p1;
+  p1 = (Ponto*) malloc(sizeof(Ponto));
+
+  iniciarPonto(p1, 1, 10);
+
+  printf("%i\n", p1->v1);
+  printf("%i\n", p1->v2);
+
+  liberarPonto(p1);
+
+  return 0;
+}
+
--- C_dir/estrDados/10_25/07_10_25/atvd3.c
+++ C_dir/estrDados/10_25/07_10_25/atvd3.c
@@ -1,3 +1,58 @@
 /*
  * 3) Crie uma estrutura para armazenar dados de alunos (nome e cidade). Crie uma função para criar/adicionar alunos usando malloc.
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+  char nome[30];
+  char cidade[20];
+} Aluno;
+
+void criarAluno(Aluno *aluno, char nome[30], char cidade[20]){
+  strcpy(aluno->nome, nome);
+  strcpy(aluno->cidade, cidade);
+}
+
+void limparAluno(Aluno *aluno){
+  if(aluno == NULL){
+    return;
+  }
+  free(aluno);
+}
+
+void adicionarAluno(Aluno *aluno, char nome[30], char cidade[20], int *nAlunos){
+  Aluno *novoAluno;
+
+  if((novoAluno = (Aluno*) malloc(*nAlunos * sizeof(Aluno))) == NULL){
+    free(novoAluno);
+    return;
+  }
+
+  strcpy(novoAluno->nome, nome);
+  strcpy(novoAluno->cidade, cidade);
+
+  if((aluno = (Aluno*) realloc(aluno, *nAlunos++)) == NULL){
+    printf("Falha ao realocar memoria.");
+    return;
+  }
+
+}
+
+int main(){
+
+  Aluno *aluno;
+  int quantidadeAlunos = 1;
+
+  aluno = (Aluno*) malloc(quantidadeAlunos * sizeof(Aluno));
+
+  criarAluno(aluno, "Claudio", "Corrente");
+
+  printf("%s", aluno->cidade);
+
+  limparAluno(aluno);
+
+  return 0;
+}
